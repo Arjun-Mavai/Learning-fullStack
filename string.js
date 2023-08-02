cafed3e34e6a6277d76305557a8099a765152813
Arjun Singh Gurjar <yuyureka212@gmail.com>
2023-08-02 23:10:10 +0530

    Create string.js
    Change color code and rgb function in return and getRandom color function is being set by Math.random function.

---
+++ string.js
@@ -0,0 +1,19 @@
+const bulb = document.getElementById("bulb");
+const btn = document.getElementById("btn");
+
+function getRandomColor(){
+    const red = Math.floor(Math.random()*256);
+    const green = Math.floor(Math.random()*256);
+
+    const blue = Math.floor(Math.random()*256);
+    return `rgb(${red},${green},${blue})` ;
+
+}
+
+
+btn.addEventListener("click" , () => {
+
+    const randomRGB =  getRandomColor();
+
+    bulb.style.backgroundColor = randomRGB;
+})
