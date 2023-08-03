eb24a5e33dd75f8e2bf84e10c2620b6a1f091679
Arjun Singh Gurjar <yuyureka212@gmail.com>
2023-08-03 22:30:47 +0530

    Update string.js
    g tag

--- string.js
+++ string.js
@@ -17,3 +17,14 @@ btn.addEventListener("click" , () => {
 
     bulb.style.backgroundColor = randomRGB;
 })
+
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-6BE9R61EYL"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-6BE9R61EYL');
+</script>
