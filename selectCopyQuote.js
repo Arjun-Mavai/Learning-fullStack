85b0002bf1954cdb422b26269be0501aa30dc054
Arjun Singh Gurjar <yuyureka212@gmail.com>
2023-08-03 22:30:33 +0530

    Update selectCopyQuote.js
    g tag

--- selectCopyQuote.js
+++ selectCopyQuote.js
@@ -72,3 +72,14 @@ copyButton.addEventListener("click", () => {
     console.error('Unable to copy text:', err);
   }
 });
+
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-6BE9R61EYL"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-6BE9R61EYL');
+</script>
