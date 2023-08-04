06ce675d026266151a3a63ff755672b23437b532
Arjun Singh Gurjar <yuyureka212@gmail.com>
2023-08-04 17:45:18 +0530

    Create app.js
    theme change functionality using javascript

---
+++ app.js
@@ -0,0 +1,26 @@
+ 
+
+
+
+  fetch("config.json")
+  .then((response) => response.json())
+  .then((config) => {
+    const themeLink = document.getElementById("theme");
+
+    // Apply the theme from the configuration file
+    themeLink.href = config.theme === "dark" ? "dark.css" : "light.css";
+
+    // Add event listeners to update the theme based on user input
+    const darkBtn = document.getElementById("darkBtn");
+    const lightBtn = document.getElementById("lightBtn");
+
+    darkBtn.addEventListener("click", () => {
+      themeLink.href = "dark.css";
+      config.theme = "dark";
+    });
+
+    lightBtn.addEventListener("click", () => {
+      themeLink.href = "light.css";
+      config.theme = "light";
+    });
+  });
