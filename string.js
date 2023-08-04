1401802672bb27622228fd549d73b58e612af467
Arjun Singh Gurjar <yuyureka212@gmail.com>
2023-08-04 17:58:45 +0530

    Update string.js
    removed data analytics code

--- string.js
+++ string.js
@@ -19,12 +19,4 @@ btn.addEventListener("click" , () => {
 })
 
 
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-6BE9R61EYL"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-6BE9R61EYL');
-</script>
+ 
